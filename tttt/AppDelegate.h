933cb71095deeaf419c0160f9ebf884207624b03
WuLiuyan <wuliuyan@huayugroup360.com>
2017-07-31 17:37:32 +0800

    Initial Commit

---
+++ tttt/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  tttt
+//
+//  Created by Yong Yang on 2017/7/31.
+//  Copyright © 2017年 wly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
